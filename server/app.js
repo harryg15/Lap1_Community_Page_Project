5b9819422ed77ad8b6d97ffb00c6ed399e84759f
Rameez <rameez0khawaja@gmail.com>
2022-06-27 10:38:38 +0100

    added file reading

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const port = 3000;
+const fs = require("fs");
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// To do: Create a route for retrieving all quotes
+app.get("/data/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    fs.readFile(`./json files/${id}.json`, "utf8", (err, jsonString) => {
+    if (err) {
+        console.log("Error reading file from disk:", err);
+    }
+    try {
+        const data = JSON.parse(jsonString);
+        res.send(data)
+    } catch (err) {
+        console.log("Error parsing JSON string:", err);
+    }
+    });
+})
+
+// const customer = {
+//     name: "Newbie Co.",
+//     order_count: 0,
+//     address: "Po Box City",
+// }
+// const jsonString = JSON.stringify(customer)
+// fs.writeFile('./newdata.json', jsonString, err => {
+//     if (err) {
+//         console.log('Error writing file', err)
+//     } else {
+//         console.log('Successfully wrote file')
+//     }
+// })
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
