861e051eacc2bd3d17bd7ad0e73b348f2b07f91b
Rameez <rameez0khawaja@gmail.com>
2022-06-30 16:36:11 +0100

    testing

--- client/bundle2.js
+++ client/bundle2.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let section = document.querySelector("section")
 
 var emoji1Update = function (){}
@@ -187,3 +188,5 @@ function sendArray(button, newBox, id){
 }
 
 module.exports = {init, mainLoop, postFunction, fetchComments, createComments, sendArray, emoji1Update, emoji2Update, emoji3Update}
+
+},{}]},{},[1]);
